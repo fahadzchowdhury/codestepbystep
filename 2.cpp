e69c71f713eb2df806582b5675b0e934b7f103be
Fahad Zaman Chowdhury <fahadzchowdhury@Fahad-Laptop.localdomain>
2021-02-23 23:54:01 +0600

    Question 2

---
+++ 2.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+    cout<<"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"<<endl;
+    cout<<"|| FEAR THE TREE! ||"<<endl;
+    cout<<"////////////////////"<<endl;
+}
