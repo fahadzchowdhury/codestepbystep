dedcb4c9abe154dd80cd534511b89c723dd9476a
Fahad Zaman Chowdhury <fahadzchowdhury@Fahad-Laptop.localdomain>
2021-03-02 01:10:37 +0600

    deleting numbered files

--- 2.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(void)
-{
-    cout<<"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\"<<endl;
-    cout<<"|| FEAR THE TREE! ||"<<endl;
-    cout<<"////////////////////"<<endl;
-}
