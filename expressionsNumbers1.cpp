94becb2e7790ad1d28fc1ff0c3e8fc84da2cdeb8
Fahad Zaman Chowdhury <fahadzchowdhury@Fahad-Laptop.localdomain>
2021-03-02 02:57:22 +0600

    solution of expressionsNumbers1

---
+++ expressionsNumbers1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+    cout<<1 + 2 * 3 + 7 * 2 % 5<<endl;
+    cout<<1 * 2 + 3 * 5 % 4<<endl;
+    cout<<1 + 8 % 3 * 2 - 9<<endl;
+    cout<<2.0 * 2.4 + 2.25 * 4.0 / 2.0<<endl;
+    cout<<7 / 3 * 1.2 + 3 / 2<<endl;
+    cout<<2.0 + 10 / 3 * 2.5 - 6 / 4<<endl;
+    /*
+        11
+        5
+        -4
+        9.3
+        3.4
+        8.5
+    */
+}
